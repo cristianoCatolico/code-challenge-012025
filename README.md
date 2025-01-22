2e2da3c8be37fd0a3572931bcff10120644d022a
Cristiano Catolico <jlozano@uni.edu.pe>
2025-01-22 10:31:12 -0500

    [fix]: clean

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# code-challenge-012025
+## Table of Contents
+- [Installation](#Installation)
+- [Usage](#Usage)
+
+# Instalation
+Must create .env file with the following variables
+
+```
+ENV=
+SERVER_PORT=
+POLYGON_API_KEY=
+POLYGON_URL=
+```
+
+Then execute
+```
+npm i
+```
+
+# Usage
+For usage execute
+
+```
+npm run start
+```
--- src/server.js
+++ src/server.js
@@ -2,17 +2,15 @@ require('dotenv').config();
 
 const express = require('express')
 const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
 const tickerRoutes = require('./routes/tickersRoute')
 const config = require('./utils/config/config');
 const logger = require('./utils/logger/loggerCaller');
 
 const app = express()
 
-app.use(express.json()); // Format Json Data
-app.use(cors()); // Allow comunication of all origins
-app.use(express.static('public')); // Allow static files on public (For Swagger)
+app.use(express.json());
+app.use(cors()); 
+app.use(express.static('public'));
 
 app.use('/api', tickerRoutes);
 
@@ -23,13 +21,3 @@ const server = app.listen(config.web.port, () => {
     const content = `URL: http://localhost:${port}.\nEnviroment: ${config.enviroment}.`;
     logger.info(message, logger.types.SYSTEM, 'express', 'runServer', content);
   });
-
-
-/*
-https://codewithmatt.hashnode.dev/understanding-the-building-blocks-of-a-web-application-routes-controllers-services-repositories-and-databases
-https://github.com/cerciber/node-general-architecture/blob/develop/src/utils/statics/paths.js
-https://betterstack.com/community/guides/logging/how-to-install-setup-and-use-winston-and-morgan-to-log-node-js-applications/
-https://medium.com/@bjprajapati381/using-winston-for-logging-in-node-js-applications-d15302947c28
-https://dev.to/cuongnp/swagger-nodejs-express-a-step-by-step-guide-4ob
-https://github.com/danielschmitz/express-solid/blob/master/src/api/foo.js
-*/
